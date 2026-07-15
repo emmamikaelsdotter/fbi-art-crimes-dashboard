51d27a51c6566f9e948c26eed594c0b6f97fa0aa
Emma Mikaelsdotter Andersson <emmamikaelsdotterandersson@gmail.com>
2026-07-15 20:28:24 +0200

    perf: set 5-minute staleTime on QueryClient to avoid refetching on focus

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,13 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
